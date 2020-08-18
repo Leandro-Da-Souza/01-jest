595e54bbd0f3681ba50e8564d4faa994f02aae8f
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-08-18 11:14:52 +0200

    finish capitalize function and test

--- exercises.js
+++ exercises.js
@@ -8,7 +8,12 @@ retrive = () => {
     return saved;
 };
 
+capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 module.exports = {
     store,
     retrive,
+    capitalize,
 };
--- exercises.test.js
+++ exercises.test.js
@@ -1,6 +1,6 @@
 let exercises = require('./exercises.js');
 
-const { store, retrive } = exercises;
+const { store, retrive, capitalize } = exercises;
 
 // 2a vilka testfall har vi?
 it('should store a value', () => {
@@ -23,5 +23,28 @@ it('should store a value and return the most recently stored value', () => {
     let expected = value;
     let actual = retrive();
 
+    expect(actual).toBe(expected);
+});
+
+it('should capitalize giraffe', () => {
+    // arrange
+    const string = 'Giraffe';
+
+    // act
+    let actual = capitalize('giraffe');
+
+    // assert
+    expect(actual).toBe(string);
+});
+
+it('should capitalize monster', () => {
+    // arrange
+    let string = 'monster';
+    let expected = 'Monster';
+
+    //act
+    let actual = capitalize(string);
+
+    //assert
     expect(actual).toBe(expected);
 });
